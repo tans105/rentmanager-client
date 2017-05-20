7ef9ba91470fa8630f10db715fec0802ed214f23
tans105 <tanmayawasthi105@gmail.com>
2017-05-20 18:00:08 +0530

    separate controller for app

---
+++ app/app-controller.js
@@ -0,0 +1,54 @@
+/**
+ * Created by tanmay on 20/5/17.
+ */
+'use strict';
+
+appModule.controller('myAppCtrl', function ($scope, $cookies, $state, $rootScope) {
+
+    console.log("App controller reporting on duty");
+
+    $rootScope.$on('$stateChangeSuccess',
+        function (event, toState, toParams, fromState, fromParams) {
+            var cookieData = $cookies.getObject('cookieData');
+
+            if (angular.isUndefined(cookieData) || cookieData == null || !cookieData) {
+                console.log("OOPS! no cookie");
+                $scope.clearLoginStatus();
+                $state.go('login');
+            }
+        });
+
+    $scope.isLoggedIn = false;
+    $scope.role = '';
+    $scope.loggedInUser = '';
+
+    $scope.changeLoginStatus = function (val) {
+        $scope.isLoggedIn = val.isLoggedIn;
+        $scope.role = val.role;
+        $scope.loggedInUser = val.loggedInUser;
+
+    }
+    $scope.clearLoginStatus = function () {
+        $scope.isLoggedIn = false;
+        $scope.role = '';
+        $scope.loggedInUser = '';
+    }
+
+
+    $scope.logout = function () {
+        $cookies.remove('cookieData');
+        $scope.clearLoginStatus();
+        $state.go('login');
+
+    }
+
+    $scope.changeColor = function (bool) {
+        if (bool === true) {
+            $scope.hoverColour = {'font-size': 'x-large', 'margin-top': '4px', 'color': 'black'};
+        } else if (bool === false) {
+            $scope.hoverColour = {'font-size': 'x-large', 'margin-top': '4px', 'color': '#1fa67b'};
+        }
+    };
+
+
+});
