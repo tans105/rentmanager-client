12e1ab1c6e5ffd871bebab0e7fd9270fa43e1de8
tans105 <tanmayawasthi105@gmail.com>
2017-05-23 14:31:31 +0530

    User management started with tablular structure

--- app/app-modules/home/home-modules/userManagement/userManagement.js
+++ app/app-modules/home/home-modules/userManagement/userManagement.js
@@ -5,5 +5,43 @@ appModule.controller('UserManagementCtrl', function ($scope, $state, loginServic
     if (cookieData) {
         $scope.message = "User Management page";
         $scope.activateModule("userManagement");
+        $scope.users = [
+            { 'userName':'Tanmay Awasthi',
+                'userId': 'TA001',
+                'role': 'Admin'},
+            { 'userName':'Manisha Ojha',
+                'userId': 'MO001',
+                'role': 'Admin'},
+            { 'userName':'Pushkar Dwivedi',
+                'userId': 'PD002',
+                'role': 'Tenant'},
+            { 'userName':'Daxil Shah',
+                'userId': 'DS002',
+                'role': 'Tenant'},
+            { 'userName':'Nishit Suthar',
+                'userId': 'NS002',
+                'role': 'Tenant'},
+        ];
+        $scope.addRow = function(){
+            $scope.users.push({ 'userName':$scope.userName, 'userId': $scope.userId, 'role':$scope.role });
+            $scope.userName='';
+            $scope.userId='';
+            $scope.role='';
+        };
+        $scope.removeRow = function(name){
+            var index = -1;
+            var comArr = eval( $scope.users );
+            for( var i = 0; i < comArr.length; i++ ) {
+                if( comArr[i].name === name ) {
+                    index = i;
+                    break;
+                }
+            }
+            if( index === -1 ) {
+                alert( "Something gone wrong" );
+            }
+            $scope.users.splice( index, 1 );
+        };
+
     }
 });
