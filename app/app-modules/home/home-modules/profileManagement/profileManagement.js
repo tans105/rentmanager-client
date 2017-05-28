96ce8ef6368b55dd2e273ab88c9e35c25e69d892
tans105 <tanmayawasthi105@gmail.com>
2017-05-28 16:53:50 +0530

    CRUD operations working for profile management

--- app/app-modules/home/home-modules/profileManagement/profileManagement.js
+++ app/app-modules/home/home-modules/profileManagement/profileManagement.js
@@ -1,16 +1,18 @@
 'use strict';
 
 
-appModule.controller('ProfileManagementCtrl', function ($scope, $state, $cookies, fetchProfileDetailsService, $parse) {
+appModule.controller('ProfileManagementCtrl', function ($scope, $state, $cookies, personalDetailsService, $parse,cfpLoadingBar) {
     var cookieData = $cookies.getObject('cookieData');
     if (cookieData) {
         $scope.message = "Profile Management";
         $scope.activateModule("profileManagement");
 
         var profileFetchSuccess = function (response) {
+            cfpLoadingBar.complete();
+            console.log(response);
             if (response.data.success) {
-                console.log(response.data.formSchema);
-                console.log(response.data.personalDetails);
+                // console.log(response.data.formSchema);
+                // console.log(response.data.personalDetails);
                 $scope.formSchema = response.data.formSchema;
                 $scope.personalDetails = response.data.personalDetails;
 
@@ -27,17 +29,53 @@ appModule.controller('ProfileManagementCtrl', function ($scope, $state, $cookies
                 }
             }
         }
-        fetchProfileDetailsService.fetchProfile(cookieData.token, profileFetchSuccess);
+        var profileStoreSuccess = function (response) {
+            cfpLoadingBar.complete();
+            console.log(response);
+        }
+        $scope.storeInfo = function () {
+            for (var property in $scope.personalDetails) {
+                if ($scope.personalDetails.hasOwnProperty(property)) {
+                    if (property == 'userId') {
+                        continue;
+                    }
+                    else {
+                        $scope.personalDetails[property] = $scope[property];
+                    }
+                }
+            }
+            cfpLoadingBar.start();
+            personalDetailsService.storeProfile(cookieData.token, $scope.personalDetails, profileStoreSuccess);
+        }
+        cfpLoadingBar.start();
+        personalDetailsService.fetchProfile(cookieData.token, profileFetchSuccess);
     }
 
 });
 
 
-appModule.service('fetchProfileDetailsService', function ($http) {
+appModule.service('personalDetailsService', function ($http) {
     this.fetchProfile = function (token, callback) {
         $http({
-            url: "http://localhost:8080/api/profile",
+            url: "http://localhost:8080/api/profile/fetchProfile",
             method: "GET",
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        })
+            .then(function (response) {
+                    callback(response);
+                },
+                function (response) {
+                    callback();
+                });
+    }
+
+    this.storeProfile = function (token, personalDetails, callback) {
+        $http({
+            url: "http://localhost:8080/api/profile/saveProfile",
+            method: "POST",
+            data: personalDetails,
             headers: {
                 'Authorization': 'Bearer ' + token
             }
